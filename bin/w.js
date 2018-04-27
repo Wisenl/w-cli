2bce8cf7dd4ab5f66ad43fe65fbe9225c6a79260
Wisen <wisenl@icloud.com>
2018-04-27 19:40:23 +0800

    add cmd

--- bin/w.js
+++ bin/w.js
@@ -1,9 +1,28 @@
-#!/user/bin/env node
+#!/usr/bin/env node
 
 const program = require('commander')
+const init = require('../lib/cmd/init')
+const list = require('../lib/cmd/list')
+program
+  .version(require('../package').version, '-v, --version')
 
 program
-    .version(require('../package').version)
-    .usage('<command> [option]')
+  .command('init')
+  .description('初始化项目')
+  .alias('a')
+  .action(() => {
+      init()
+  })
+program
+  .command('list')
+  .description('查看列表')
+  .alias('l') // 简写
+  .action(() => {
+    list()
+  })
+program
+  .parse(process.argv);
 
-
+if(!program.args.length){
+  program.help()
+}
